4897fc14109d6481010a05223c9f938ce64e5ce4
Anyone <anytguy@gmail.com>
2024-11-24 23:55:50 +0200

    Changed path to api.js

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 Install sudo apt install node-typescript
+
+node ./server/server.js
--- scripts/tracking.js
+++ scripts/tracking.js
@@ -1,4 +1,4 @@
-import { getDeliveries } from "./api.js"; // Імпортуємо функцію отримання даних
+import { getDeliveries } from "./server/api.js"; // Імпортуємо функцію отримання даних
 
 document.addEventListener("DOMContentLoaded", () => {
   const searchButton = document.getElementById("search-button");
